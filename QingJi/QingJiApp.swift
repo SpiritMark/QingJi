a5359dae5dfdf7e447694545134813c693abd624
刘路长 <liuluchang@bytedance.com>
2025-03-21 15:00:12 +0800

    Initial Commit

---
+++ QingJi/QingJiApp.swift
@@ -0,0 +1,17 @@
+//
+//  QingJiApp.swift
+//  QingJi
+//
+//  Created by ByteDance on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct QingJiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
